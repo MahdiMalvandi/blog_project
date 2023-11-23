22e5c9bea9f74111d9ceb2bfcf064f7445a101ae
mahdi <mahdimalvandi6@gmail.com>
2023-11-23 20:30:37 +0330

    develope blog page

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -5,5 +5,11 @@ urlpatterns = [
     # pages
     path('', views.home, name='home'),
     path('blogs/', views.blogs, name='blogs'),
-    path('blogs/<str:slug>/', views.get_blog_detail, name='blog page')
+    path('blogs/<str:slug>/', views.get_blog_detail, name='blog page'),
+    path('blogs/category/<str:category_name>/', views.get_category_posts, name='category posts'),
+
+    path('services/', views.services, name='services'),
+    path('about_us/', views.about_us, name='about'),
+    path('contact/', views.contact, name='contact'),
+
 ]
--- blog_app/views.py
+++ blog_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from .models import *
 
+
 # Create your views here.
 
 def home(request):
@@ -9,12 +10,14 @@ def home(request):
     }
     return render(request, 'blog_app/index.html', context)
 
+
 def blogs(request):
     posts = Post.objects.select_related('author', 'category').all()
     context = {
         'posts': posts
     }
     return render(request, 'blog_app/blog.html', context)
+
 
 def get_blog_detail(request, slug):
     post = get_object_or_404(Post, slug=slug)
@@ -23,3 +26,24 @@ def get_blog_detail(request, slug):
         'post': post
     }
     return render(request, 'blog_app/post.html', context)
+
+
+def get_category_posts(request, category_name):
+    posts = Post.objects.select_related('author', 'category').filter(category__text=category_name)
+    context = {
+        'category': category_name,
+        'posts': posts
+    }
+    return render(request, 'blog_app/filtered_post.html', context)
+
+
+def services(request):
+    return render(request, 'blog_app/services.html')
+
+
+def about_us(request):
+    return render(request, 'blog_app/about.html')
+
+
+def contact(request):
+    return render(request, 'blog_app/contact.html')
