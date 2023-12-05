7bf016997aecf3d3c22f60525571d356eca0b193
mahdi <mahdimalvandi6@gmail.com>
2023-12-05 15:35:06 +0330

    add admin panel app

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'blog_app.apps.BlogConfig',
+    'admin_panel.apps.AdminPanelConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- blog_project/urls.py
+++ blog_project/urls.py
@@ -20,7 +20,8 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
+    path('admin/', include('admin_panel.urls'), name='admin_panel'),
     path('', include('blog_app.urls'), name='blog_app app'),
     path('ckeditor', include("ckeditor_uploader.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
